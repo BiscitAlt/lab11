19b636b7d980cc2c7dae6176de778745d04d50b6
BiscitAlt <thitiputt.pe@gmail.com>
2025-12-26 15:35:29 +0700

    lab11_1 completed

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,47 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+
+int main(){
+    string grade;
+    int score;
+    cout << "Press Enter 3 times to reveal your future.";
+    for(int i=0; i<3; i++){
+        cin.ignore();
+    }
+    srand(time(0));
+    score = rand()%9;
+    switch (score)
+    {
+    case 8:
+        grade = "B+";
+        break;
+    case 7:
+        grade = "B";
+        break;
+    case 6:
+        grade = "C+";
+        break;
+    case 5:
+        grade = "C";
+        break;
+    case 4:
+        grade = "D+";
+        break;
+    case 3:
+        grade = "D";
+        break;
+    case 2:
+        grade = "F";
+        break;
+    case 1:
+        grade = "W";
+        break;
+    default:
+        grade = "A";
+        break;
+    }
+    cout << "You will get " << grade <<" in this 261102.";
+    return 0;
+}
